41e78541dbe6026a3f0693be9f7a2fdced3a26f1
Ednan Dias <ednan.741@gmail.com>
2022-02-14 14:54:40 -0300

    adding first migration and configuration with typeorm

---
+++ src/models/Class.ts
@@ -0,0 +1,25 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('class')
+export default class Class {
+  @PrimaryColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  duration: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
