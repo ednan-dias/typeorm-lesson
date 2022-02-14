d7eaf39d2cf700e2ead7dc08a985334e217bb2f4
Ednan Dias <ednan.741@gmail.com>
2022-02-14 17:58:15 -0300

    fixing migration of class

--- src/database/migrations/1644864642057-CreateClass.ts
+++ src/database/migrations/1644864642057-CreateClass.ts
@@ -5,7 +5,7 @@ export default class CreateClass1644864642057 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "class" ("id" uuid NOT NULL, "name" character varying NOT NULL, "duration" integer NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_0b9024d21bdfba8b1bd1c300eae" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "class" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "duration" integer NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_0b9024d21bdfba8b1bd1c300eae" PRIMARY KEY ("id"))`,
     );
   }
 
